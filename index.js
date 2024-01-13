3ca1ab86cff930b14e91722c4ee2ffd32b5bcefa
Angel <juliojunior11@hotmail.com>
2024-01-13 17:33:53 -0400

    cambio de proveedor DB

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const cargarPais = async () => {
 
-const apiUrl = "https://api-paises-production.up.railway.app/v1/random"
+const apiUrl = "https://country-api-8rtc.onrender.com/v1/random"
 
 const res = await fetch(apiUrl);
 
@@ -12,14 +12,14 @@ datos.forEach( (pais, index) =>{
   toHtml += `
         <div class="bandera" >
         <img src="${pais.urlImg}" class="card-img-top" alt="Bandera de ${
-          pais.nombre
+          pais.pais
         }" ${index > 20 ? 'loading="lazy"' : ""}>
         <div class="overlay">
           <i class="bi bi-people-fill"></i>
           <p>${pais.gentilicio.join('<br>')}</p>
         </div>
         <div class="pais text-bg-dark align-items-center flex-column">
-          <p class="text-bg-secondary paisF">${pais.nombre}</p>
+          <p class="text-bg-secondary paisF">${pais.pais}</p>
           <p>${pais.capital}</p>
         </div>
       </div>
